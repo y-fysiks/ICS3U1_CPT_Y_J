4cea250eaa1513bf34ccab6fce5d1511b1bb70d4
y-fysiks <y-fysiks@users.noreply.github.com>
2020-11-05 16:19:42 -0500

    working start game prompt

--- src/game/Controller.java
+++ src/game/Controller.java
@@ -1,4 +1,9 @@
 package game;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void startGame(ActionEvent actionEvent) {
+        Main.startGame = true;
+    }
 }
--- src/game/Main.java
+++ src/game/Main.java
@@ -3,7 +3,9 @@ package game;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -12,48 +14,58 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-//hi
-//asdasdp
-//I think I know what's wrong
-//nvm
-//hi lol
-//ah
-//u could just
-//change the address
-//wait imma search it up
+import java.io.File;
+
 public class Main extends Application{
-    Stage window;
     StackPane layout1, layout2; //layout1 = menu, layout2 = game
     Button btn;
     Scene menu,game;
+    public static boolean startGame = false;
     private int width = 1280, height = 720;
-    @Override
+
     public void start(Stage primaryStage) throws Exception{
-        Image player = new Image("file:player.png");
-        Image playerDestroyed = new Image("file:player destroyed.png");
-        Image enemy1 = new Image("file:enemy.png");
-        window = primaryStage;
-        window.setTitle("Space Invaders");
+        //setup/initialization
+        primaryStage.setTitle("Space Invaders");
+
+        Image player = new Image(new File("player.png").toURI().toString(), 80, 36, true, false);
+        Image playerDestroyed = new Image(new File("playerDestroyed.png").toURI().toString());
+        Image enemy1 = new Image(new File("enemy.png").toURI().toString());
+        Group root = new Group();
+        game = new Scene(root);
+        primaryStage.setScene(game);
+        System.out.println(player.getHeight());
+        Canvas canvas = new Canvas(1280, 720);
+        root.getChildren().add(canvas);
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
         layout1 = new StackPane();
-        layout2 = new StackPane();
         btn = new Button();
         btn.setText("Start Game!");
-        btn.setOnAction(e -> window.setScene(game));
+        btn.setOnAction(e -> primaryStage.setScene(game));
         layout1.getChildren().add(btn);
-        layout2.setBackground(new Background(new BackgroundFill(Color.rgb(34,47,62), CornerRadii.EMPTY, Insets.EMPTY)));
         menu = new Scene(layout1,width,height);
-        game = new Scene(layout2, width, height);
-        window.setScene(menu);
-        window.show();
+        primaryStage.setScene(menu);
+        primaryStage.show();
         new AnimationTimer() {
             @Override
             public void handle(long now) {
-
+                gc.setFill( Color.RED );
+                gc.setStroke( Color.BLACK );
+                gc.setLineWidth(2);
+                Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
+                gc.setFont( theFont );
+                gc.fillText( "Hello, World!", 60, 50 );
+                gc.strokeText( "Hello, World!", 60, 50 );
+                gc.drawImage(player, 100, 660);
             }
         }.start();
 
+        primaryStage.show();
 
     }
 
