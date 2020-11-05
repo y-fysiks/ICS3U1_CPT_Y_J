be483cae0cd538d1df2fdb6a83beb6ab90b3cb3f
y-fysiks <y-fysiks@users.noreply.github.com>
2020-11-05 16:24:34 -0500

    working start game prompt

--- src/game/Main.java
+++ src/game/Main.java
@@ -34,14 +34,18 @@ public class Main extends Application{
         Image player = new Image(new File("player.png").toURI().toString(), 80, 36, true, false);
         Image playerDestroyed = new Image(new File("playerDestroyed.png").toURI().toString());
         Image enemy1 = new Image(new File("enemy.png").toURI().toString());
+
         Group root = new Group();
         game = new Scene(root);
         primaryStage.setScene(game);
-        System.out.println(player.getHeight());
+
         Canvas canvas = new Canvas(1280, 720);
         root.getChildren().add(canvas);
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Font TimesNewRoman = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
+        gc.setFont( TimesNewRoman );
 
         layout1 = new StackPane();
         btn = new Button();
@@ -50,18 +54,18 @@ public class Main extends Application{
         layout1.getChildren().add(btn);
         menu = new Scene(layout1,width,height);
         primaryStage.setScene(menu);
+
         primaryStage.show();
+
+
         new AnimationTimer() {
             @Override
             public void handle(long now) {
                 gc.setFill( Color.RED );
                 gc.setStroke( Color.BLACK );
                 gc.setLineWidth(2);
-                Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
-                gc.setFont( theFont );
-                gc.fillText( "Hello, World!", 60, 50 );
-                gc.strokeText( "Hello, World!", 60, 50 );
-                gc.drawImage(player, 100, 660);
+
+
             }
         }.start();
 
