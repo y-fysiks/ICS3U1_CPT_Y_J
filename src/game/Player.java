5c2ad35fbc44ddb16bae25a33f64681c623d3769
y-fysiks <y-fysiks@users.noreply.github.com>
2020-11-05 21:24:37 -0500

    Update lots of stuff

--- src/game/Player.java
+++ src/game/Player.java
@@ -10,7 +10,7 @@ public class Player {
     final int y = 660;
     int points = 0;
     Bullet bullet = new Bullet();
-    boolean destroyed = false;
+    int destroyed = 0;
 
     Image player = new Image(new File("player.png").toURI().toString(), 80, 36, true, false);
     Image playerDestroyed = new Image(new File("playerDestroyed.png").toURI().toString(), 80, 36, true, false);
@@ -20,7 +20,21 @@ public class Player {
     }
 
     public void update() {
-        Main.gc.drawImage(player, x, y);
+        if (destroyed == 0) {
+            Main.gc.drawImage(player, x, y);
+            bullet.update();
+        } else {
+            Main.gc.drawImage(playerDestroyed, x, y);
+            bullet.update();
+            destroyed -= 1;
+        }
+    }
+    public void destroy() {
+        destroyed = 50;
+        x = 100;
+    }
 
+    public boolean isHit(Bullet bullet) {
+        return bullet.y > y + 10 && bullet.y < y + 36 && bullet.x > x && bullet.x < x + 80;
     }
 }
